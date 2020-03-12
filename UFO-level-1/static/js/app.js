699844809186537c6cf906943326a516a1e8d525
philipstubbs13 <philipstubbs13@gmail.com>
2020-03-11 21:07:21 -0500

    working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,68 +1,59 @@
 // from data.js
 const tableData = data;
 
-// Find the table body node where we will append the data to.
+// HTML elements/nodes
 const tableBody = document.getElementById('ufo-table').getElementsByTagName('tbody')[0];
+const filterBtn = document.getElementById('filter-btn');
+const dateFilterInput = document.getElementById("datetime");
+const tr = tableBody.getElementsByTagName("tr");
 
-const createTable = data => {
+// Function to create/populate table with data.
+const createTable = (data, tableBody) => {
   // Loop through the items in the data array.
   for (let i = 0; i < data.length; i++) {
     const tableRowData = data[i];
     // Insert a row in the table at the last row
     const newRow = tableBody.insertRow();
 
-    // Insert a cell in the row at index 0
-    const dateCell = newRow.insertCell(0);
-    const cityCell = newRow.insertCell(1);
-    const stateCell = newRow.insertCell(2);
-    const countryCell = newRow.insertCell(3);
-    const shapeCell = newRow.insertCell(4);
-    const durationCell = newRow.insertCell(5);
-    const commentsCell = newRow.insertCell(6);
-
+    // Insert a cell in the row at index.
     // Create text nodes for each.
-    const dateText  = document.createTextNode(tableRowData.datetime);
-    const cityText = document.createTextNode(tableRowData.city);
-    const stateText = document.createTextNode(tableRowData.state);
-    const countryText = document.createTextNode(tableRowData.country);
-    const shapeText = document.createTextNode(tableRowData.shape);
-    const durationText = document.createTextNode(tableRowData.durationMinutes.toString());
-    const commentsText = document.createTextNode(tableRowData.comments);
-
     // Append a text node to the cell.
-    dateCell.appendChild(dateText);
-    cityCell.appendChild(cityText);
-    stateCell.appendChild(stateText);
-    countryCell.appendChild(countryText);
-    shapeCell.appendChild(shapeText);
-    durationCell.appendChild(durationText);
-    commentsCell.appendChild(commentsText);
+    Object.entries(tableRowData).forEach(([key, value], index) => {
+      let cell = newRow.insertCell(index)
+      let textNode = document.createTextNode(value.toString());
+      cell.appendChild(textNode);
+    })
   }
 }
 
-createTable(tableData);
+// Call the create table function.
+createTable(tableData, tableBody);
 
 
-// Filter button
-const filterBtn = document.getElementById('filter-btn');
-
-// Date filter value
-const dateFilterInput = document.getElementById("datetime");
 let dateFilterInputValue = document.getElementsByTagName("input")[0].value;
-console.log(dateFilterInputValue);
  
 filterBtn.addEventListener("click", () => {
-  console.log('Filter button clicked...')
-  console.log(dateFilterInputValue);
-  if (dateFilterInputValue) {
-    const filteredData = tableData.filter(row => row.datetime === dateFilterInputValue);
-    console.log(filteredData);
-    createTable(filteredData); 
+  for (let i = 0; i < tr.length; i++) {
+    if (dateFilterInputValue) {
+      // Hide the row initially.
+      tr[i].style.display = "none";
+      let td = tr[i].getElementsByTagName("td");
+      for (let j = 0; j < td.length; j++) {
+        let cell = tr[i].getElementsByTagName("td")[0];
+        if (cell) {
+          if (cell.innerHTML.toUpperCase().indexOf(dateFilterInputValue) > -1) {
+            tr[i].style.display = "";
+            break;
+          } 
+        }
+      }
+    } else {
+      tr[i].style.display = "";
+    }
   }
 });
 
-dateFilterInput.addEventListener('input', updateDateTime);
+// Function that gets fired when value of the datetime input changes.
+dateFilterInput.addEventListener('input', e => dateFilterInputValue = e.target.value);
 
-function updateDateTime(e) {
-  dateFilterInputValue = e.target.value;
-}
+
