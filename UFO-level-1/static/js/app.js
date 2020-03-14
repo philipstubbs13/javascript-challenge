453c3845015a5f94b802b4eb8ca5d0044caf1530
philipstubbs13 <philipstubbs13@gmail.com>
2020-03-14 09:41:19 -0500

    working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,8 +4,8 @@ const tableData = data;
 // HTML elements/nodes
 const tableBody = document.getElementById('ufo-table').getElementsByTagName('tbody')[0];
 const filterBtn = document.getElementById('filter-btn');
-const dateFilterInput = document.getElementById("datetime");
-const tr = tableBody.getElementsByTagName("tr");
+const tableRows = tableBody.getElementsByTagName('tr');
+const inputs = document.getElementsByTagName('input');
 
 // Function to create/populate table with data.
 const createTable = (data, tableBody) => {
@@ -19,41 +19,71 @@ const createTable = (data, tableBody) => {
     // Create text nodes for each.
     // Append a text node to the cell.
     Object.entries(tableRowData).forEach(([key, value], index) => {
-      let cell = newRow.insertCell(index)
+      let cell = newRow.insertCell(index);
       let textNode = document.createTextNode(value.toString());
       cell.appendChild(textNode);
-    })
+    });
   }
-}
+};
 
-// Call the create table function.
+// Call the create table function to build the table and insert data into the table.
 createTable(tableData, tableBody);
 
+// State to hold the values of the user input fields.
+let inputValues = {
+  datetime: '',
+  city: '',
+  state: '',
+  country: '',
+  shape: '',
+  comment: '',
+};
 
-let dateFilterInputValue = document.getElementsByTagName("input")[0].value;
- 
-filterBtn.addEventListener("click", () => {
-  for (let i = 0; i < tr.length; i++) {
-    if (dateFilterInputValue) {
-      // Hide the row initially.
-      tr[i].style.display = "none";
-      let td = tr[i].getElementsByTagName("td");
-      for (let j = 0; j < td.length; j++) {
-        let cell = tr[i].getElementsByTagName("td")[0];
-        if (cell) {
-          if (cell.innerHTML.toUpperCase().indexOf(dateFilterInputValue) > -1) {
-            tr[i].style.display = "";
-            break;
-          } 
+// Get the keys of the input elements.
+// and use those keys to set the name attribute for each input element.
+const inputKeys = Object.keys(inputValues);
+for (let i = 0; i < inputs.length; i++) {
+  let input = inputs[i];
+  input.setAttribute('name', inputKeys[i]);
+}
+
+// Event listener that gets fired when the filter button is clicked.
+filterBtn.addEventListener('click', () => {
+  // filterValues are the values of what the user typed inside the form input fields.
+  const filterValues = Object.values(inputValues);
+  // Convert list of tr html elements to an array so that we can iterate over the table rows using forEach.
+  let tableRowsArray = Array.from(tableRows);
+  tableRowsArray.forEach(row => {
+    // Display each row initially.
+    row.style.display = '';
+    // Get the table data for each row.
+    let td = row.getElementsByTagName('td');
+    // Conert table data to an array so that we can iterate over it.
+    let tdArray = Array.from(td);
+    tdArray.forEach((td, tdIndex) => {
+      let cell = row.getElementsByTagName('td')[tdIndex];
+      if (cell) {
+        if (filterValues[tdIndex]) {
+          console.log(`in table: ${cell.innerHTML.toUpperCase()}, filter: ${filterValues[tdIndex]}`);
+          // If the row is not already filtered out and matches the filter from the user input,
+          // then display the row.
+          if (
+            row.style.display !== 'none' &&
+            cell.innerHTML.toUpperCase().indexOf(filterValues[tdIndex].toUpperCase()) > -1
+          ) {
+            row.style.display = '';
+          } else {
+            row.style.display = 'none';
+          }
         }
       }
-    } else {
-      tr[i].style.display = "";
-    }
-  }
+    });
+  });
 });
 
-// Function that gets fired when value of the datetime input changes.
-dateFilterInput.addEventListener('input', e => dateFilterInputValue = e.target.value);
+// Function that gets fired when value of input changes.
+document.addEventListener('input', handleFieldChange);
 
-
+function handleFieldChange(e) {
+  inputValues[e.target.name] = e.target.value;
+}
