3e9a7727a81fafdf7df661354620b816e10751cc
philipstubbs13 <philipstubbs13@gmail.com>
2020-03-14 14:59:31 -0500

    using d3 now

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,29 +1,33 @@
 // from data.js
 const tableData = data;
 
+// Get list of countries and states;
+let countries = [];
+let states = [];
+tableData.forEach(td => {
+  if (countries.indexOf(td.country) === -1) {
+    countries.push(td.country);
+  }
+  if (states.indexOf(td.state) === -1) {
+    states.push(td.state);
+  }
+});
+
 // HTML elements/nodes
-const tableBody = document.getElementById('ufo-table').getElementsByTagName('tbody')[0];
-const filterBtn = document.getElementById('filter-btn');
-const tableRows = tableBody.getElementsByTagName('tr');
-const inputs = document.getElementsByTagName('input');
+const table = d3.select('table');
+const tableBody = table.select('tbody');
+const filterBtn = d3.select('#filter-btn');
 
 // Function to create/populate table with data.
 const createTable = (data, tableBody) => {
   // Loop through the items in the data array.
-  for (let i = 0; i < data.length; i++) {
-    const tableRowData = data[i];
+  data.forEach(ufo => {
     // Insert a row in the table at the last row
-    const newRow = tableBody.insertRow();
+    let newRow = tableBody.append('tr');
 
-    // Insert a cell in the row at index.
-    // Create text nodes for each.
-    // Append a text node to the cell.
-    Object.entries(tableRowData).forEach(([key, value], index) => {
-      let cell = newRow.insertCell(index);
-      let textNode = document.createTextNode(value.toString());
-      cell.appendChild(textNode);
-    });
-  }
+    // Insert a cell in the row at index, create text nodes for each, append a text node to the cell.
+    Object.entries(ufo).forEach(([key, value]) => newRow.append('td').text(value.toString()));
+  });
 };
 
 // Call the create table function to build the table and insert data into the table.
@@ -42,25 +46,27 @@ let inputValues = {
 // Get the keys of the input elements.
 // and use those keys to set the name attribute for each input element.
 const inputKeys = Object.keys(inputValues);
-for (let i = 0; i < inputs.length; i++) {
-  let input = inputs[i];
-  input.setAttribute('name', inputKeys[i]);
-}
+d3.selectAll('input').each(function(d, i) {
+  this.setAttribute('name', inputKeys[i]);
+});
+
+// Function that gets fired when value of input changes.
+d3.selectAll('input').on('change', event => (inputValues[d3.event.target.name] = d3.event.target.value));
 
 // Event listener that gets fired when the filter button is clicked.
-filterBtn.addEventListener('click', () => {
+filterBtn.on('click', () => {
   // filterValues are the values of what the user typed inside the form input fields.
   const filterValues = Object.values(inputValues);
-  // Convert list of tr html elements to an array so that we can iterate over the table rows using forEach.
-  let tableRowsArray = Array.from(tableRows);
-  tableRowsArray.forEach(row => {
+  const tableRows = tableBody.selectAll('tr');
+  tableRows.each(function() {
+    let row = this;
     // Display each row initially.
     row.style.display = '';
     // Get the table data for each row.
     let td = row.getElementsByTagName('td');
-    // Conert table data to an array so that we can iterate over it.
+    // Convert table data to an array so that we can iterate over it.
     let tdArray = Array.from(td);
-    tdArray.forEach((td, tdIndex) => {
+    tdArray.forEach(function(td, tdIndex) {
       let cell = row.getElementsByTagName('td')[tdIndex];
       if (cell) {
         if (filterValues[tdIndex]) {
@@ -80,10 +86,3 @@ filterBtn.addEventListener('click', () => {
     });
   });
 });
-
-// Function that gets fired when value of input changes.
-document.addEventListener('input', handleFieldChange);
-
-function handleFieldChange(e) {
-  inputValues[e.target.name] = e.target.value;
-}
